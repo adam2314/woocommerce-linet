0156f88c4054a41ed54b7db146d5aab3ee01ae49
adam <adam@linet.org.il>
2025-05-22 17:23:49 +0300

    sync back last one for 2day

--- classes/class-wc-li-inventory.php
+++ classes/class-wc-li-inventory.php
@@ -5,7 +5,7 @@ Plugin URI: https://github.com/adam2314/woocommerce-linet
 Description: Integrates <a href="http://www.woothemes.com/woocommerce" target="_blank" >WooCommerce</a> with the <a href="http://www.linet.org.il" target="_blank">Linet</a> accounting software.
 Author: Speedcomp
 Author URI: http://www.linet.org.il
-Version: 3.6.8
+Version: 3.6.9
 Text Domain: linet-erp-woocommerce-integration
 Domain Path: /languages/
 WC requires at least: 2.2
@@ -1207,7 +1207,7 @@ class WC_LI_Inventory
 
 
     foreach($products as $product)
-      $result = self::WpItemSync($product[0], $logger);
+      $result = self::WpItemSync($product, $logger);
     if ($result)
       echo json_encode(
         array(
