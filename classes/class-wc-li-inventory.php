fc43a24607f021a94fcf11b5b57d94187378b3cd
adam <adam@linet.org.il>
2025-05-22 16:33:30 +0300

    the bird is the ward

--- classes/class-wc-li-inventory.php
+++ classes/class-wc-li-inventory.php
@@ -5,7 +5,7 @@ Plugin URI: https://github.com/adam2314/woocommerce-linet
 Description: Integrates <a href="http://www.woothemes.com/woocommerce" target="_blank" >WooCommerce</a> with the <a href="http://www.linet.org.il" target="_blank">Linet</a> accounting software.
 Author: Speedcomp
 Author URI: http://www.linet.org.il
-Version: 3.6.7
+Version: 3.6.8
 Text Domain: linet-erp-woocommerce-integration
 Domain Path: /languages/
 WC requires at least: 2.2
@@ -1198,13 +1198,16 @@ class WC_LI_Inventory
 
     global $wpdb;
 
-      $products = $wpdb->get_results($wpdb->prepare("SELECT ID FROM {$wpdb->posts} as p WHERE " .
-      "(p.post_type='product' OR p.post_type='product_variation') AND " .
+      $products = $wpdb->get_col($wpdb->prepare("SELECT ID FROM {$wpdb->posts} p WHERE " .
       " p.post_status = 'publish' AND " .
-      " p.ID=%d OR p.post_parent=%d" , $post_id));
+      "(p.post_type='product' OR p.post_type='product_variation') AND " .
+      " (p.ID=%d OR p.post_parent=%d)" 
+      
+      , $post_id, $post_id));
+
 
     foreach($products as $product)
-    $result = self::WpItemSync($product->ID, $logger);
+      $result = self::WpItemSync($product[0], $logger);
     if ($result)
       echo json_encode(
         array(
